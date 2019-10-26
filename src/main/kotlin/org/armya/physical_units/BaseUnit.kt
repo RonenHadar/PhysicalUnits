844b5b7b27a8f6acff1cf0cd790c89ef76a518f3
RonenHadar <zlochenko@yahoo.com>
2019-10-26 17:03:07 +0300

    cleanup enum

--- src/main/kotlin/org/armya/physical_units/BaseUnit.kt
+++ src/main/kotlin/org/armya/physical_units/BaseUnit.kt
@@ -2,27 +2,17 @@ package org.armya.physical_units
 
 enum class BaseUnit: Comparable<BaseUnit> {
     Meter {
-        override val order: Int = 0
     },
     Second {
-        override val order: Int = 1
     },
     Kilogram {
-        override val order: Int = 2
     },
     Ampere {
-        override val order: Int = 3
     },
     Kelvin {
-        override val order: Int = 4
     },
     Mole {
-        override val order: Int = 5
     },
     Kandela {
-        override val order: Int = 6
     };
-
-    // maybe no need for order, needs to see
-    abstract val order: Int
 }
